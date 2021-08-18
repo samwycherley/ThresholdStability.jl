88b9779b249b16be6cb3a752817bc98f20f889bc
Sam Wycherley <samvpwycherley@gmail.com>
2021-08-18 11:24:05 +0100

    Modify description

--- src/indicator.jl
+++ src/indicator.jl
@@ -4,13 +4,6 @@ export indicator
     indicator(y, b; indregion=:above, ineq=:nonstrict)
 
 Indicator function of default form ``1\{y\\geq b\}``. The inequality can be made strict and the direction of inequality can be reversed.
-
-# Arguments
-- `indregion=:above` (default): calculates ``1\{y\\geq b\}`` or ``1\{y>b\}``.
-- `indregion=:below`: calculates ``1\{y\\leq b\}`` or ``1\{y<b\}``.
-- `ineq=:nonstrict` (default): calculates ``1\{y\\geq b\}`` or ``1\{y\\leq b\}``.
-- `ineq=:strict`: calculates ``1\{y>b\}`` or ``1\{y<b\}``.
-
 """
 function indicator(y, b; indregion = :above, ineq=:nonstrict)
     # Indicator function. The default mode ('above') returns 𝟏{y ≥ b}. The other available mode, 'below', returns 𝟏{y ≤ b}. Set 'ineq' to 'strict' to get 𝟏{y > b} or 𝟏{y < b}.
