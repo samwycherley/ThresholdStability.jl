798d4bc97048610fc60a4b5f5b8b619aa3caef1b
Sam Wycherley <samvpwycherley@gmail.com>
2021-08-18 18:15:09 +0100

    fix reexport

--- src/ThresholdStability.jl
+++ src/ThresholdStability.jl
@@ -5,17 +5,33 @@ using JuMP, SumOfSquares, DynamicPolynomials, MultivariatePolynomials
 using CSDP
 using Combinatorics, LinearAlgebra
 
+import Reexport
+Reexport.@reexport using HybridSystems
+
 """
     spectral_radius(A::AbstractMatrix)
 
 Calculates the spectral radius of matrix `A`, using a utility from [SwitchOnSafety.jl](https://github.com/blegat/SwitchOnSafety.jl).
 """
 spectral_radius(A::AbstractMatrix) = ρ(A)  # from SwitchOnSafety.jl
-export spectral_radius
+"""
+    jsr(Σ::AbstractVector{<:AbstractMatrix})
 
-import Reexport
-Reexport.@reexport using HybridSystems
-Reexport.@reexport using SwitchOnSafety: soslyapb
+Calculates an upper bound on the joint spectral radius of a set of matrices `Σ`, using a utility from [SwitchOnSafety.jl](https://github.com/blegat/SwitchOnSafety.jl).
+"""
+jsr(Σ::AbstractVector{<:AbstractMatrix}) = soslyapb(discreteswitchedsystem(Σ), 2, optimizer_constructor = optimizer_with_attributes(CSDP.Optimizer, MOI.Silent() => true))[2]
+
+jsr(Σ::AbstractVector{<:AbstractMatrix}, d, optimizer) = soslyapb(discreteswitchedsystem(Σ), d, optimizer_constructor = optimizer)[2]
+
+"""
+    cjsr(Σ::AbstractVector{<:AbstractMatrix}, G::AbstractAutomaton)
+
+Calculates an upper bound on the constrained joint spectral radius of a switching system `(Σ, G)`, using a utility from [SwitchOnSafety.jl](https://github.com/blegat/SwitchOnSafety.jl).
+"""
+cjsr(Σ::AbstractVector{<:AbstractMatrix}, G::AbstractAutomaton) = soslyapb(discreteswitchedsystem(Σ, G), 2, optimizer_constructor = optimizer_with_attributes(CSDP.Optimizer, MOI.Silent() => true))[2]
+cjsr(Σ::AbstractVector{<:AbstractMatrix}, G::AbstractAutomaton, d, optimizer) = soslyapb(discreteswitchedsystem(Σ, G), d, optimizer_constructor = optimizer)[2]
+
+export spectral_radius, cjsr, jsr
 
 include("indicator.jl")
 include("automaton_constructor.jl")
