b0322f1fa3d30a57d6dd6c2679925945e6ced7ca
Sam Wycherley <samvpwycherley@gmail.com>
2021-08-27 14:02:58 +0100

    change make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,21 +1,20 @@
 using ThresholdStability
 using Documenter
 
-DocMeta.setdocmeta!(ThresholdStability, :DocTestSetup, :(using ThresholdStability); recursive=true)
+# DocMeta.setdocmeta!(ThresholdStability, :DocTestSetup, :(using ThresholdStability); recursive=true)
 
-makedocs(;
-    modules=[ThresholdStability],
+makedocs(
+#    modules=[ThresholdStability],
     authors="Sam Wycherley",
-    repo="https://github.com/samwycherley/ThresholdStability.jl/blob/{commit}{path}#{line}",
+#    repo="https://github.com/samwycherley/ThresholdStability.jl/blob/{commit}{path}#{line}",
     sitename="ThresholdStability.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://samwycherley.github.io/ThresholdStability.jl",
-        assets=String[],
+    format=Documenter.HTML(prettyurls=get(ENV, "CI", "false") == "true"
+#        canonical="https://samwycherley.github.io/ThresholdStability.jl",
+#        assets=String[],
     ),
     pages=[
         "Home" => "index.md",
-        "Stability" => "stability.md"
+#        "Stability" => "stability.md"
 #        "CKSVAR" => "cksvar.md"
     ],
 )
